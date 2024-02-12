06c718cd93eb778f7a962d6f8b3172589155d312
talpadeanil <talpadeanil5@gmail.com>
2024-02-12 15:04:04 +0530

    added

--- script3.js
+++ script3.js
@@ -1,8 +1,6 @@
 console.log('i am in python')
 console.log('i am in git ')
 console.log(' a am in kolkatta')
-console.log(' i am in changing from ')
 
 
-
-console.log(' hello am i an new code from login_feature')
+console.log(' i am in new code from login_feature')
