a77be587c47a83770188dd39cdd0d5aca8c825f3
talpadeanil <talpadeanil5@gmail.com>
2024-03-24 14:38:58 +0530

    added

--- script2.js
+++ script2.js
@@ -1,13 +1,1 @@
-console.log('i am learning js')
-console.log('i am learning js')
-
-console.log(' a am in git')
-console.log('hiiiii')
-
-console.log('manish')
-console.log('shankar')
-
-console.log('i am js')
-console.log('i am in advance excel')
-console.log('i am js'
-)
+console.log('hello')
